d9bd854567bcf41aaa789d6c1528895fb9cfd0c3
Yogeshwar Dan Charan <danchara@adobe.com>
2017-03-17 20:08:35 +0530

    tried running javadoc

---
+++ src/main/java/receiver/Receiver.java
@@ -0,0 +1,14 @@
+package receiver;
+
+/**
+ * @author Yogeshwar Dan Charan
+ * Created by danchara on 3/15/2017.
+ *
+ *  For receiving data from different sources : http, mqtt etc
+ */
+
+public interface Receiver {
+
+    void receive();
+
+}
---
+++ src/main/java/receiver/mqtt/MqttReceiver.java
@@ -0,0 +1,16 @@
+package receiver.mqtt;
+
+import receiver.Receiver;
+
+/**
+ * @author Yogeshwar Dan Charan  3/15/2017
+ *
+ *  To subscribe to a MQTT broker on certain topics and recieve messages/events (e.g. open, close ,failed) .
+ */
+public class MqttReceiver implements Receiver {
+    public void receive() {
+
+    }
+
+
+}
