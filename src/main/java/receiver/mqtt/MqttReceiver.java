40a52ff99ffff7ada2400f6f697e376028ec5fc9
Yogeshwar Dan Charan <danchara@adobe.com>
2017-03-21 07:50:03 +0530

    tested initial publish/subscribe locally , next step : to put this stuff inside docker container

--- src/main/java/receiver/mqtt/MqttReceiver.java
+++ src/main/java/receiver/mqtt/MqttReceiver.java
@@ -2,15 +2,54 @@ package receiver.mqtt;
 
 import receiver.Receiver;
 
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
 /**
- * @author Yogeshwar Dan Charan  3/15/2017
- *
- *  To subscribe to a MQTT broker on certain topics and recieve messages/events (e.g. open, close ,failed) .
+ * @autCreated by danchara on 3/15/2017.
  */
 public class MqttReceiver implements Receiver {
     public void receive() {
 
     }
 
+    public static void main(String[] args) {
+
+        String topic        = "test";
+        String content      = "Message from MqttPublishSample";
+        int qos             = 2;
+        String broker       = "tcp://yogi-mosquitto.tk:1883";
+        String clientId     = "JST";
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            connOpts.setUserName("rcl_1");
+            connOpts.setPassword("p8cxapj".toCharArray());
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: "+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+            System.exit(0);
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
+    }
 
 }
